ffdc18ca9996d567f4746a411e8e6534ca242fde
umeyuki <umeyuki1326+github@gmail.com>
2014-08-30 15:08:06 +0900

    とりあえずwork

--- agent.pl
+++ agent.pl
@@ -6,7 +6,26 @@ use utf8;
 
 use Config::Pit;
 use Net::Twitter::Lite::WithAPIv1_1;
+use Encode;
 use DDP;
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
+use Pod::Usage qw/pod2usage/;
+use LWP::UserAgent;
+
+my $ua = LWP::UserAgent->new();
+
+=head
+
+Option
+  date: YAPC前夜祭の開催日付
+
+=cut
+
+GetOptions(
+    \my %opt, qw/
+    date=s
+/) or pod2usage(1);
+
 
 my $config = pit_get(
     "api.twitter.com",
@@ -29,6 +48,48 @@ my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
 $nt->access_token($config->{token});
 $nt->access_token_secret($config->{token_secret});
 
-my $res = $nt->search({q=>'#yapcasia',count => 5, lang => 'ja'});
+sub main {
+    my $cond = join(" OR ", qq/#yapcasia YAPC yapc/);
+    $cond = $cond . " -rt/-via exclude:retweets filter:links since:$opt{date}";
 
-p $res;
+    my $since_id = 0;
+
+    while ( my $res = &search_tweet( $nt, $cond, $since_id ) ) {
+        for my $tweet ( @{ $res->{statuses} } ) {
+            my $url = $tweet->{entities}->{urls}->[0]->{url};
+            if ($url) {
+                my $res = &get_post($url);
+
+                # 短縮URL リダイレクト先URL
+                my $post_url = $res->request->uri;
+                my $title    = &get_title( $res->content );
+
+                #TODO instagramとか外す
+                next unless  $title;
+                print sprintf( "<a href='%s'>%s</a>", $post_url, $title ) . "\n";
+            }
+        }
+        $since_id = $res->{search_metadata}->{max_id};
+        last unless $res->{search_metadata}->{next_results};
+    }
+}
+
+
+sub search_tweet {
+    my ( $nt, $cond, $since_id) = @_;
+    $nt->search({q => $cond, since_id => $since_id ,count => 20, lang => 'ja'});
+}
+
+sub get_post {
+    my $url = shift;
+    $ua->get($url);
+}
+
+sub get_title {
+    my $html = shift;
+    my ($title) = $html =~ m!<title>(.+?)</title>!i;
+    return $title;
+}
+
+
+&main();
